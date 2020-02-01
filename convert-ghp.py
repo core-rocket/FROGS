16184291e2e3df1939d7b225136227c03670c585
sksat <sksat@sksat.net>
2020-02-01 15:23:21 +0900

    generate restrict table by altitude

--- convert-ghp.py
+++ convert-ghp.py
@@ -5,6 +5,8 @@ import csv
 import pymap3d as pm
 import json
 
+RESTRICT_ALTITUDE = 914.0
+
 launch_lat = 34.679730
 launch_lon = 139.438373
 launch_alt = 39.4422
@@ -25,7 +27,7 @@ def read_ghp(f):
     data = {}
     for row in reader:
         wspeed = float(row["wspeed"])
-        case = {"wdir": float(row["wdir"]), "ghp_e": float(row["ghp_e"]), "ghp_n": float(row["ghp_n"])}
+        case = {"wdir": float(row["wdir"]), "ghp_e": float(row["ghp_e"]), "ghp_n": float(row["ghp_n"]), "max_altitude": float(row["max_altitude"])}
         if not wspeed in data.keys():
             data[wspeed] = []
         data[wspeed].append(case)
@@ -48,8 +50,52 @@ def ghp2js(data):
     #print(output)
     return output
 
+def check_restrict(ghp):
+    for wspeed in ghp.keys():
+        cases = ghp[wspeed]
+        print("check %f m/s..." % wspeed)
+        for case in cases:
+            check_case(case)
+
+def check_case(case):
+    case["restrict_altitude"] = (case["max_altitude"] < RESTRICT_ALTITUDE)
+
+def show_restrict_table(ghp):
+    print("| 風向風速 |", end="")
+    for wspeed in ghp.keys():
+        print(" %s m/s |" % str(wspeed), end="")
+    print("")
+    print("|:-|", end="")
+    for ws in ghp.keys():
+        print("-|", end="")
+    print("")
+
+    wdir_list = set([])
+    for wspeed in ghp.keys():
+        for case in ghp[wspeed]:
+            wdir_list.add(case["wdir"])
+    wdir_list = sorted(wdir_list)
+    #print(wdir_list)
+    for wdir in wdir_list:
+        print("| %s deg |" % str(wdir), end = "")
+        for wspeed in ghp.keys():
+            for case in ghp[wspeed]:
+                if wdir == case["wdir"]:
+                    output = "不可("
+                    if case["restrict_altitude"] == False:
+                        output += "高度)"
+                    else:
+                        output = ""
+                    output += " |"
+                    print(output, end="")
+        print("")
+
 with open(sys.argv[1]) as f:
     ghp = read_ghp(f)
+    check_restrict(ghp)
+
     js = ghp2js(ghp)
     ghp_js = open("ghp-output.js", "w")
     ghp_js.write(js)
+
+    show_restrict_table(ghp)
